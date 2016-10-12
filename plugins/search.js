52b04528155f7d87c5e3db9f31100d03f8717872
Roger Raymond <roger@lynch-raymond.com>
2016-10-12 09:14:38 -0400

    Update google search for better results

--- plugins/search.js
+++ plugins/search.js
@@ -3,6 +3,7 @@ const Promise = require('bluebird');
 const _       = require('lodash');
 const request = Promise.promisifyAll(require('request'));
 const config = require('./search_config.json');
+const google = require('google');
 
 const sites = [
     [ "mdn",     "developer.mozilla.org " ]
@@ -24,76 +25,60 @@ function getSiteUrl(siteName) {
 }
 
 var search = Promise.method(function(data, userData, bot) {
-  //var userText = "<@"+slack.hook.user_id+"|"+slack.hook.user_name+">";
-  //var searchURL = 'https://ajax.googleapis.com/ajax/services/search/web';
-	var searchURL = 'https://www.googleapis.com/customsearch/v1';
   var site = data.matches[1];
   var query = data.matches[2];
   var siteUrl = getSiteUrl(site);
-	var qs = {
-		"cx": config.searchId,
-			"key": config.apikey,
-			"v": "1.0",
-			"safe": "high",
-			"filter": "1",
-			"fields": "items(displayLink,fileFormat,formattedUrl,htmlFormattedUrl,htmlSnippet,htmlTitle,kind,link,mime,snippet,title)",
-			"num": 5,
-			"q": query
-	};
+
 	if(siteUrl) {
-		qs.siteSearch = siteUrl
+		query = `site:${siteUrl} ${query}`
 	}
-  return request.getAsync({
-    url: searchURL,
-		qs: qs
-  })
-  .spread(function(response, body){
-    if(response.statusCode==200) {
-		  var results = JSON.parse(body);
-		  if(results.items != null && !!results.items.length) {
-				var items = results.items;
-        var resultUrl = items[0].link;
-        var resultTitle = items[0].title;
-				var resultSnippet = items[0].snippet.replace('\n',' ');
-        var resultMore = 'https://google.com/search?q='+query;
-        bot.sendMessage({
-					username: "Search Results",
-					icon_emoji: ":mag_right:",
-					text: [
-						"<"+resultUrl+"|"+resultTitle+">",
-						resultSnippet,
-						"<"+resultMore+"|See More Results\u2026>"
-					].join('\n')
-				});
-		  }
-    }
-  });
+
+	google(query, (err, res) => {
+		if(err) {
+			return false;
+		}
+		if(res.links.length) {
+			let result = res.links[0];
+			bot.sendMessage({
+				username: "Search Results",
+				icon_emoji: ":mag_right:",
+				text: [
+					"<"+result.link+"|"+result.title+">",
+					result.description.replace(/\n/g, ' '),
+					"<"+res.url+"|See More Results\u2026>"
+				].join('\n')
+			});
+		}
+		res.links.forEach((link) => {
+
+		});
+		return true;
+	});
+});
+
+var helpText = `
+*NAME*
+Google Search - Searches google for your phrase and returns the first result.
+`;
+var help = Promise.method(function(data, userData, bot) {
+	bot.sendMessage({text: helpText})
 });
 
 exports.load = function(registry) {
-  var helpText = 'Perform a search';
-  registry.register(
-    //plugin name
-    'google search',
-    // trigger regex
-    new RegExp("^[`!](g|google) (.*?)$",'im'),
-    // function to run
-    search,
-    // help text
-    helpText
-  );
+  registry.respond(
+		new RegExp("(g|google) (.*?)$",'im'),
+		search
+	);
   // register the other search triggers
   sites.forEach(function(s) {
-    registry.register(
-      //plugin name
-      s[0]+' search',
-      // trigger regex
-      new RegExp("^[`!]("+s[0]+") (.*?)$",'im'),
-      // function to run
-      search,
-      // help text
-      helpText
-    );
+    registry.respond(
+			new RegExp("("+s[0]+") (.*?)$",'im'),
+			search
+		);
   });
-  return true;
-}
+
+	registry.help('Google Search - Searches google for your phrase and returns the first result. `search help` for more.');
+	registry.hear(/^search help$/, help);
+	return true;
+
+};
